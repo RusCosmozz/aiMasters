fa49f3d3c70682cab9551d150445c54f805a2872
ErmakovKS <KSeErmakov@sberbank.ru>
2023-04-04 15:32:15 +0300

    Initial commit

---
+++ src/main/java/ru/dungeon/aiMasters/AiMastersApplication.java
@@ -0,0 +1,13 @@
+package ru.dungeon.aiMasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dungeon/aiMasters/AiMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dungeon.aiMasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
